5f2335063c03d3b5a4ad8a78537ab09568d8f38b
tazeen_22 <tazeen.2229@gmail.com>
2024-08-22 22:23:29 -0700

    commit

--- calculator.js
+++ calculator.js
@@ -1,48 +1,9 @@
-let display = document.getElementById('display');
-let currentInput = '';
-let operator = '';
-let firstOperand = '';
+let display = document.getElementById("display");
+let currentInput = "";
+let operator = "";
+let firstOperand = "";
+let lastOperator = "";
+let lastOperand = "";
 
-function appendNumber(number) {
-currentInput += number;
-display.value = currentInput;
-}
 
-function appendOperator(op) {
-if (currentInput === '') return;
-firstOperand = currentInput;
-operator = op;
-currentInput = '';
-}
 
-function calculate() {
-if (currentInput === '' || firstOperand === '') return;
-let result;
-switch (operator) {
-case '+':
-result = parseFloat(firstOperand) + parseFloat(currentInput);
-break;
-case '-':
-result = parseFloat(firstOperand) - parseFloat(currentInput);
-break;
-case '*':
-result = parseFloat(firstOperand) * parseFloat(currentInput);
-break;
-case '/':
-result = parseFloat(firstOperand) / parseFloat(currentInput);
-break;
-default:
-return;
-}
-display.value = result;
-currentInput = result;
-firstOperand = '';
-operator = '';
-}
-
-function clearDisplay() {
-currentInput = '';
-firstOperand = '';
-operator = '';
-display.value = '';
-}
